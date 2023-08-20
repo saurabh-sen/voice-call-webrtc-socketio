7d4b97202f44c7ddb3361ae68b91e45c11cc3b0f
Saurabh Sen <saurabh.sen.cs19@ggits.net>
2023-08-20 12:36:29 +0530

    FIX: added turn servers

--- public/constants.js
+++ public/constants.js
@@ -4,9 +4,14 @@ const google_stun2 = "stun:stun2.l.google.com:19302";
 const google_stun3 = "stun:stun3.l.google.com:19302";
 const google_stun4 = "stun:stun4.l.google.com:19302";
 const stun_servers = [
-    {
-      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
-    },
-  ];
+  {
+    url: "turn:numb.viagenie.ca",
+    credential: "muazkh",
+    username: "webrtc@live.com",
+  },
+  {
+    urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+  },
+];
 const server_url = "http://localhost:8000/";
 export { stun_servers, server_url };
